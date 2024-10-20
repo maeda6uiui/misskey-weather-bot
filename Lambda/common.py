a9ff6ce5a3efe968ccd3e33e07d6ea4d56be3060
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-10-20 18:34:35 +0900

    Change to class

--- Lambda/common.py
+++ Lambda/common.py
@@ -1,138 +1,199 @@
 import pandas as pd
 import requests
-from logging import Logger
+from logging import getLogger,Logger
 from misskey import Misskey
 
-def get_weather_forecast(api_key:str,q:str,days:int,lang:str,logger:Logger)->dict[str,pd.DataFrame]:
+class WeatherForecastPoster(object):
     """
-    天気予報のデータを取得する
-
-    Parameters
-    ----------
-    api_key: str
-        Weather APIのAPIキー
-    q: str
-        クエリパラメータ
-    days: int
-        天気予報を取得する日数
-    lang: str
-        天気予報の言語
-    logger: Logger
-        ロガー
-
-    Returns
-    ----------
-    dict[str,DataFrame]
-        location: 位置データ
-        daily: 1日ごとのデータ
-        hourly: 1時間ごとのデータ
+    天気予報をMisskeyに投稿するクラス
     """
-    response=requests.get(
-        "https://api.weatherapi.com/v1/forecast.json",
-        headers={
-            "key": api_key
-        },
-        params={
-            "q": q,
-            "days": days,
-            "lang": lang
+    def __init__(
+        self,
+        weather_api_key:str,
+        misskey_server_url:str,
+        misskey_access_token:str,
+        logger:Logger=None):
+        """
+        Parameters
+        ----------
+        weather_api_key: str
+            Weather APIのAPIキー
+        misskey_server_url: str
+            MisskeyサーバーのURL
+        misskey_access_token: str
+            Misskeyのアクセストークン
+        logger (optional): Logger
+            ロガー
+        """
+        self._weather_api_key=weather_api_key
+        self._mk=Misskey(address=misskey_server_url,i=misskey_access_token)
+
+        if logger is not None:
+            self._logger=logger
+        else:
+            self._logger=getLogger(__name__)
+
+    def _get_weather_forecast(self,q:str,days:int)->dict[str,pd.DataFrame]:
+        """
+        天気予報のデータを取得する
+
+        Parameters
+        ----------
+        q: str
+            クエリパラメータ
+        days: int
+            天気予報を取得する日数
+
+        Returns
+        ----------
+        dict[str,DataFrame]
+            location: 位置データ
+            daily: 1日ごとの天気予報データ
+            hourly: 1時間ごとの天気予報データ
+        """
+        response=requests.get(
+            "https://api.weatherapi.com/v1/forecast.json",
+            headers={
+                "key": self._weather_api_key
+            },
+            params={
+                "q": q,
+                "days": days,
+                "lang": "ja"
+            }
+        )
+        if response.status_code!=200:
+            raise RuntimeError(f"Weather APIの実行に失敗しました: {response.status_code}")
+        
+        data=response.json()
+
+        location=data["location"]
+        data_location={
+            "name": [location["name"]],
+            "region": [location["region"]],
+            "country": [location["country"]]
         }
-    )
-    if response.status_code!=200:
-        logger.error(f"Weather APIの実行に失敗しました: {response.status_code}")
-        return None
+        df_location=pd.DataFrame(data_location)
+
+        data_daily={
+            "date": [],
+            "maxtemp_c": [],
+            "mintemp_c": [],
+            "avgtemp_c": [],
+            "condition": [],
+            "sunrise": [],
+            "sunset": []
+        }
+        data_hourly={
+            "time": [],
+            "temp_c": [],
+            "condition": []
+        }
+
+        for forecastday in data["forecast"]["forecastday"]:
+            #1日ごとのデータ
+            date=forecastday["date"]
+            maxtemp_c=forecastday["day"]["maxtemp_c"]
+            mintemp_c=forecastday["day"]["mintemp_c"]
+            avgtemp_c=forecastday["day"]["avgtemp_c"]
+            condition=forecastday["day"]["condition"]["text"]
+            sunrise=forecastday["astro"]["sunrise"]
+            sunset=forecastday["astro"]["sunset"]
+
+            data_daily["date"].append(date)
+            data_daily["maxtemp_c"].append(maxtemp_c)
+            data_daily["mintemp_c"].append(mintemp_c)
+            data_daily["avgtemp_c"].append(avgtemp_c)
+            data_daily["condition"].append(condition)
+            data_daily["sunrise"].append(sunrise)
+            data_daily["sunset"].append(sunset)
+
+            #1時間ごとのデータ
+            for hour in forecastday["hour"]:
+                time=hour["time"]
+                temp_c=hour["temp_c"]
+                condition=hour["condition"]["text"]
+
+                data_hourly["time"].append(time)
+                data_hourly["temp_c"].append(temp_c)
+                data_hourly["condition"].append(condition)
+
+        df_daily=pd.DataFrame(data_daily)
+        df_hourly=pd.DataFrame(data_hourly)
+
+        return {
+            "location": df_location,
+            "daily": df_daily,
+            "hourly": df_hourly
+        }
     
-    data=response.json()
+    def _create_misskey_note(self,text:str,visibility:str)->str:
+        """
+        Misskeyにノートを作成する
 
-    location=data["location"]
-    data_location={
-        "name": [location["name"]],
-        "region": [location["region"]],
-        "country": [location["country"]]
-    }
-    df_location=pd.DataFrame(data_location)
+        Parameters
+        ----------
+        text: str
+            ノートの内容
+        visibility: str
+            ノートの公開範囲
 
-    data_daily={
-        "date": [],
-        "maxtemp_c": [],
-        "mintemp_c": [],
-        "avgtemp_c": [],
-        "condition": [],
-        "sunrise": [],
-        "sunset": []
-    }
-    data_hourly={
-        "time": [],
-        "temp_c": [],
-        "condition": []
-    }
-
-    for forecastday in data["forecast"]["forecastday"]:
-        #1日ごとのデータ
-        date=forecastday["date"]
-        maxtemp_c=forecastday["day"]["maxtemp_c"]
-        mintemp_c=forecastday["day"]["mintemp_c"]
-        avgtemp_c=forecastday["day"]["avgtemp_c"]
-        condition=forecastday["day"]["condition"]["text"]
-        sunrise=forecastday["astro"]["sunrise"]
-        sunset=forecastday["astro"]["sunset"]
-
-        data_daily["date"].append(date)
-        data_daily["maxtemp_c"].append(maxtemp_c)
-        data_daily["mintemp_c"].append(mintemp_c)
-        data_daily["avgtemp_c"].append(avgtemp_c)
-        data_daily["condition"].append(condition)
-        data_daily["sunrise"].append(sunrise)
-        data_daily["sunset"].append(sunset)
-
-        #1時間ごとのデータ
-        for hour in forecastday["hour"]:
-            time=hour["time"]
-            temp_c=hour["temp_c"]
-            condition=hour["condition"]["text"]
-
-            data_hourly["time"].append(time)
-            data_hourly["temp_c"].append(temp_c)
-            data_hourly["condition"].append(condition)
-
-    df_daily=pd.DataFrame(data_daily)
-    df_hourly=pd.DataFrame(data_hourly)
-
-    return {
-        "location": df_location,
-        "daily": df_daily,
-        "hourly": df_hourly
-    }
-
-def create_misskey_note(address:str,access_token:str,text:str,visibility:str,logger:Logger)->str:
-    """
-    Misskeyにノートを作成する
-
-    Parameters:
-    ----------
-    address: str
-        MisskeyサーバーのURL
-    access_token: str
-        Misskeyのアクセストークン
-    text: str
-        ノートの内容
-    visibility: str
-        ノートの公開範囲
-    logger: Logger
-        ロガー
-
-    Returns
-    ----------
-    str
-        作成されたノートのID
-        エラーが発生した場合は空文字列が返される
-    """
-    note_id=""
-    try:
-        mk=Misskey(address=address,i=access_token)
-        note=mk.notes_create(text=text,visibility=visibility)
+        Returns
+        ----------
+        str
+            ノートのID
+        """
+        note=self._mk.notes_create(text=text,visibility=visibility)
         note_id=note["createdNote"]["id"]
-    except Exception as e:
-        logger.error(f"Misskeyのノート作成に失敗しました: {e}")
 
-    return note_id
+        return note_id
+    
+    def post_weather_forecast(self,q:str,visibility:str="public"):
+        """
+        天気予報をMisskeyに投稿する
+
+        Parameters
+        ----------
+        q: str
+            Weather APIを実行するときのクエリパラメータ
+        visibility: str
+            ノートの公開範囲
+        """
+        dfs=self._get_weather_forecast(q,1)
+
+        df_location=dfs["location"]
+        df_daily=dfs["daily"]
+        df_hourly=dfs["hourly"]
+
+        self._logger.debug(df_location)
+        self._logger.debug(df_daily)
+        self._logger.debug(df_hourly)
+
+        location_name=df_location["name"].item()
+
+        date=df_daily["date"].item()
+        condition=df_daily["condition"].item()
+        avgtemp_c=df_daily["avgtemp_c"].item()
+        mintemp_c=df_daily["mintemp_c"].item()
+        maxtemp_c=df_daily["maxtemp_c"].item()
+
+        text=(
+            f"{date}の{location_name}の天気予報\n\n"
+            f"{condition}\n"
+            f"{avgtemp_c}℃ (平均) / {mintemp_c}℃ (最低) / {maxtemp_c}℃ (最高)"
+        )
+        note_id=self._create_misskey_note(text,visibility)
+        self._logger.info(f"ノートID (1日ごとの天気予報): {note_id}")
+
+        text=f"{date}の{location_name}の天気予報(1時間ごと)\n\n"
+        for _,row in df_hourly.iterrows():
+            time:str=row["time"]
+            time=time.split(" ")[1]
+
+            temp_c=row["temp_c"]
+            condition=row["condition"]
+
+            text+=f"{time} / {temp_c}℃ / {condition}\n"
+
+        note_id=self._create_misskey_note(text,visibility)
+        self._logger.info(f"ノートID (1時間ごとの天気予報): {note_id}")
--- Lambda/main_local.py
+++ Lambda/main_local.py
@@ -3,14 +3,12 @@ import yaml
 from logging import getLogger,config
 from pathlib import Path
 
-from common import get_weather_forecast,create_misskey_note
+from common import WeatherForecastPoster
 
 def main(args):
     #コマンドライン引数
     weather_api_key:str=args.weather_api_key
     forecast_query_param:str=args.forecast_query_param
-    forecast_days:int=args.forecast_days
-    forecast_language:str=args.forecast_language
     misskey_server_url:str=args.misskey_server_url
     misskey_access_token:str=args.misskey_access_token
 
@@ -25,55 +23,17 @@ def main(args):
     config.dictConfig(logging_config)
     logger=getLogger(__name__)
 
-    #天気予報を取得する
-    dfs=get_weather_forecast(weather_api_key,forecast_query_param,forecast_days,forecast_language,logger)
-    if dfs is None:
-        logger.error("天気予報の取得に失敗しました")
-        return
-
-    df_location=dfs["location"]
-    df_daily=dfs["daily"]
-    df_hourly=dfs["hourly"]
-    
-    logger.debug(df_daily)
-    logger.debug(df_hourly)
-
-    #天気予報をMisskeyに投稿する
-    location_name=df_location["name"].item()
-
-    date=df_daily["date"].item()
-    condition=df_daily["condition"].item()
-    avgtemp_c=df_daily["avgtemp_c"].item()
-    mintemp_c=df_daily["mintemp_c"].item()
-    maxtemp_c=df_daily["maxtemp_c"].item()
-
-    text=(
-        f"{date}の{location_name}の天気予報\n\n"
-        f"{condition}\n"
-        f"{avgtemp_c}℃ (平均) / {mintemp_c}℃ (最低) / {maxtemp_c}℃ (最高)"
-    )
-    note_id=create_misskey_note(misskey_server_url,misskey_access_token,text,"specified",logger)
-    logger.info(f"ノートのID (1日): {note_id}")
-
-    text=f"{date}の{location_name}の天気予報(1時間ごと)\n\n"
-    for _,row in df_hourly.iterrows():
-        time:str=row["time"]
-        time=time.split(" ")[1]
-
-        temp_c=row["temp_c"]
-        condition=row["condition"]
-
-        text+=f"{time} / {temp_c}℃ / {condition}\n"
-
-    note_id=create_misskey_note(misskey_server_url,misskey_access_token,text,"specified",logger)
-    logger.info(f"ノートのID (1時間ごと): {note_id}")
+    #Misskeyに天気予報を投稿する
+    wfp=WeatherForecastPoster(weather_api_key,misskey_server_url,misskey_access_token,logger=logger)
+    try:
+        wfp.post_weather_forecast(forecast_query_param,visibility="specified")
+    except Exception as e:
+        logger.error(f"処理中にエラーが発生しました: {e}")
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("-wk","--weather-api-key",type=str)
     parser.add_argument("-q","--forecast-query-param",type=str,default="Tokyo")
-    parser.add_argument("-d","--forecast-days",type=int,default=1)
-    parser.add_argument("-l","--forecast-language",type=str,default="ja")
     parser.add_argument("-u","--misskey-server-url",type=str)
     parser.add_argument("-mk","--misskey-access-token",type=str)
     args=parser.parse_args()
