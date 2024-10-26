f0e6084ee9370a18c80c80398063a772278c4227
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-10-26 12:48:16 +0900

    Specify providers

--- Terraform/env/prod/provider.tf
+++ Terraform/env/prod/provider.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~>5.0"
     }
+    tls={
+      source = "hashicorp/tls"
+      version = "~>4.0"
+    }
+    null={
+      source = "hashicorp/null"
+      version = "~>3.0"
+    }
   }
 
   backend "s3" {
