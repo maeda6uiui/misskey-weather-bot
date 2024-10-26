98473978bade226b3c43e3ae722196a15349674b
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-10-26 18:23:09 +0900

    Add eventbridge module

---
+++ Terraform/modules/misskey_weather_bot/eventbridge/variables.tf
@@ -0,0 +1,15 @@
+variable "name_prefix" {
+  type = string
+}
+
+variable "schedule_expression" {
+  type = string
+}
+
+variable "lambda_arn" {
+  type = string
+}
+
+variable "lambda_name" {
+  type = string
+}
