4c9b5d55b11211e0cf00afa72f18601a415e6ddf
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-10-26 11:03:28 +0900

    Load emoji from file

--- Lambda/common.py
+++ Lambda/common.py
@@ -12,6 +12,7 @@ class WeatherForecastPoster(object):
         weather_api_key:str,
         misskey_server_url:str,
         misskey_access_token:str,
+        weather_conditions_filepath:str="./Data/weather_conditions.csv",
         logger:Logger=None):
         """
         Parameters
@@ -22,11 +23,15 @@ class WeatherForecastPoster(object):
             MisskeyサーバーのURL
         misskey_access_token: str
             Misskeyのアクセストークン
+        weather_conditions_filepath (optional): str
+            Weather APIで返されるコードとそれに対応する絵文字の一覧表のファイルパス
         logger (optional): Logger
             ロガー
         """
         self._weather_api_key=weather_api_key
         self._mk=Misskey(address=misskey_server_url,i=misskey_access_token)
+
+        self._df_weather_conditions=pd.read_csv(weather_conditions_filepath,encoding="utf-8")
 
         if logger is not None:
             self._logger=logger
@@ -80,14 +85,16 @@ class WeatherForecastPoster(object):
             "maxtemp_c": [],
             "mintemp_c": [],
             "avgtemp_c": [],
-            "condition": [],
+            "condition_code": [],
+            "condition_text": [],
             "sunrise": [],
             "sunset": []
         }
         data_hourly={
             "time": [],
             "temp_c": [],
-            "condition": []
+            "condition_code": [],
+            "condition_text": []
         }
 
         for forecastday in data["forecast"]["forecastday"]:
@@ -96,7 +103,8 @@ class WeatherForecastPoster(object):
             maxtemp_c=forecastday["day"]["maxtemp_c"]
             mintemp_c=forecastday["day"]["mintemp_c"]
             avgtemp_c=forecastday["day"]["avgtemp_c"]
-            condition=forecastday["day"]["condition"]["text"]
+            condition_code=forecastday["day"]["condition"]["code"]
+            condition_text=forecastday["day"]["condition"]["text"]
             sunrise=forecastday["astro"]["sunrise"]
             sunset=forecastday["astro"]["sunset"]
 
@@ -104,7 +112,8 @@ class WeatherForecastPoster(object):
             data_daily["maxtemp_c"].append(maxtemp_c)
             data_daily["mintemp_c"].append(mintemp_c)
             data_daily["avgtemp_c"].append(avgtemp_c)
-            data_daily["condition"].append(condition)
+            data_daily["condition_code"].append(condition_code)
+            data_daily["condition_text"].append(condition_text)
             data_daily["sunrise"].append(sunrise)
             data_daily["sunset"].append(sunset)
 
@@ -112,11 +121,13 @@ class WeatherForecastPoster(object):
             for hour in forecastday["hour"]:
                 time=hour["time"]
                 temp_c=hour["temp_c"]
-                condition=hour["condition"]["text"]
+                condition_code=hour["condition"]["code"]
+                condition_text=hour["condition"]["text"]
 
                 data_hourly["time"].append(time)
                 data_hourly["temp_c"].append(temp_c)
-                data_hourly["condition"].append(condition)
+                data_hourly["condition_code"].append(condition_code)
+                data_hourly["condition_text"].append(condition_text)
 
         df_daily=pd.DataFrame(data_daily)
         df_hourly=pd.DataFrame(data_hourly)
@@ -148,30 +159,26 @@ class WeatherForecastPoster(object):
 
         return note_id
     
-    def _get_condition_emoji(self,condition:str)->str:
+    def _get_condition_emoji(self,condition_code:int)->str:
         """
         天気を表す絵文字を返す
 
         Parameters
         ----------
-        condition: str
-            天気を表す文字列
+        condition_code: int
+            天気のコード
 
         Returns
         ----------
         str
             天気を表す絵文字
         """
-        if "晴" in condition:
-           return "☀"
-        elif "曇" in condition:
-            return "☁"
-        elif "雨" in condition:
-            return "☂"
-        elif "雪" in condition:
-            return "❄"
+        df_weather_condition=self._df_weather_conditions
+        record=df_weather_condition[df_weather_condition["code"]==condition_code]
+        if record.empty:
+            return ""
         
-        return ""
+        return record["emoji"].item()
     
     def post_weather_forecast(self,q:str,visibility:str="public"):
         """
@@ -197,14 +204,17 @@ class WeatherForecastPoster(object):
         location_name=df_location["name"].item()
 
         date=df_daily["date"].item()
-        condition=df_daily["condition"].item()
+        condition_code=df_daily["condition_code"].item()
+        condition_text=df_daily["condition_text"].item()
         avgtemp_c=df_daily["avgtemp_c"].item()
         mintemp_c=df_daily["mintemp_c"].item()
         maxtemp_c=df_daily["maxtemp_c"].item()
 
+        condition_emoji=self._get_condition_emoji(condition_code)
+
         text=(
             f"{date}の{location_name}の天気予報\n\n"
-            f"{self._get_condition_emoji(condition)+condition}\n"
+            f"{condition_emoji}{condition_text}\n"
             f"{avgtemp_c}℃ (平均) / {mintemp_c}℃ (最低) / {maxtemp_c}℃ (最高)"
         )
         note_id=self._create_misskey_note(text,visibility)
@@ -216,9 +226,12 @@ class WeatherForecastPoster(object):
             time=time.split(" ")[1]
 
             temp_c=row["temp_c"]
-            condition=row["condition"]
+            condition_code=row["condition_code"]
+            condition_text=row["condition_text"]
 
-            text+=f"{time} / {temp_c}℃ / {self._get_condition_emoji(condition)+condition}\n"
+            condition_emoji=self._get_condition_emoji(condition_code)
+
+            text+=f"{time} / {temp_c}℃ / {condition_emoji}{condition_text}\n"
 
         note_id=self._create_misskey_note(text,visibility)
         self._logger.info(f"ノートID (1時間ごとの天気予報): {note_id}")
