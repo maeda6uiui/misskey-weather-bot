0088c954979b70f65bc33036fbe0d411e2f6a118
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-10-26 11:47:36 +0900

    Add prod directory

---
+++ Terraform/env/prod/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "misskey-weather-bot-tfstate"
+    region = "ap-northeast-1"
+    key    = "prod.tfstate"
+  }
+
+  required_version = "~>1.9"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
